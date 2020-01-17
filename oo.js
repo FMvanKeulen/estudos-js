f34e5f905b363907a326b20946cc19726cfe61de
van Keulen <Fillipe.vanKeulen@thomsonreuters.com>
2020-01-17 13:14:10 -0300

    teste

---
+++ oo.js
@@ -0,0 +1,16 @@
+// código não executável
+
+// procedural
+processamento(valor1, valor2, valor3)
+
+// oo
+objeto = {
+    valor1, 
+    valor2, 
+    valor3, 
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() //foco passou a ser o objeto
