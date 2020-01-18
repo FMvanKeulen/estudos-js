9df9233903a9ea92fa95eb325d9c242c9d2a8b1f
Fillipe van Keulen <fmvankeulen@gmail.com>
2020-01-18 10:46:10 -0300

    Restante dos códigos

---
+++ fundamentos/comentario.js
@@ -0,0 +1,16 @@
+//comentario javascript
+console.log('linha 1')
+
+/*
+comentarios com
+quebra de lina
+*/
+
+/*
+ * teste
+ * de linha
+ * com asterisco
+ * automatico
+ */
+
+ console.log('linha 2')
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'teste'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1415
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//evitar usar nomes genéricos
+let valor = ''
+let numero = 1
+let pqp = false //produto quimico perigoso
